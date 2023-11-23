6e411d967ac02583f49a4f1efe71bd7425bbd4f5
Blooife <beusas.23@gmail.com>
2023-11-23 13:02:14 +0300

    test

---
+++ makeupStore.Services.OrderAPI/Migrations/20231121130746_UpdOrder.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace makeupStore.Services.OrderAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CouponCode",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "StripeSessionId",
+                table: "OrderHeaders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CouponCode",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Discount",
+                table: "OrderHeaders",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StripeSessionId",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ makeupStore.Services.OrderAPI/Program.cs
@@ -0,0 +1,111 @@
+using AutoMapper;
+using GreenPipes;
+using makeupStore.Services.OrderAPI;
+using makeupStore.Services.OrderAPI.Data;
+using makeupStore.Services.OrderAPI.Extensions;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference= new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id=JwtBearerDefaults.AuthenticationScheme
+                }
+            }, new string[]{}
+        }
+    });
+});
+
+builder.AddAppAuthetication();
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(new Uri("rabbitmq://localhost"));
+        cfg.ReceiveEndpoint("order-queue", e =>
+        {
+            e.PrefetchCount = 20;
+            e.UseMessageRetry(r => r.Interval(2, 100));
+        });
+        cfg.ConfigureJsonSerializer(settings =>
+        {
+            settings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
+
+            return settings;
+        });
+        cfg.ConfigureJsonDeserializer(configure =>
+        {
+            configure.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
+            return configure;
+        });
+    });
+
+});
+builder.Services.AddMassTransitHostedService();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+ApplyMigration();
+app.Run();
+
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
